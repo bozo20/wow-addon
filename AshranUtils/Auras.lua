c35fe866a4d520ec72a62add5a6535e82ffff9ed
bozo20 <molgrew@gmail.com>
2023-08-01 00:31:52 +0200

    Add files via upload
    initial commit

---
+++ AshranUtils/Auras.lua
@@ -0,0 +1,113 @@
+local f = CreateFrame("Frame")
+
+function f:OnEvent(event, ...)
+	self[event](self, event, ...)
+end
+
+function f:ADDON_LOADED(event, addOnName)
+  if addOnName == "AshranUtils" then
+    print(format("Hello %s! Auras.lua loaded.", UnitName("player")))
+  end
+end
+
+local function makeBuff(track, name, channel, banner, itemID)
+  return { track = track, name = name, channel = channel or "SAY", banner = banner or false, itemID = itemID }
+end
+
+local buffs = { --[383648] = makeBuff(false, "Erdschild"),
+                [274834] = makeBuff(false, "phalanx", nil, true),
+                [28418] = makeBuff(true, "General's Warcry 10 %", nil, true),
+                [28419] = makeBuff(true, "General's Warcry 20 %", nil, true),
+                [28420] = makeBuff(true, "General's Warcry 30 %", nil, true),
+                [171250] = makeBuff(true, "scroll of speed", nil, false, 116410),
+                [388035] = makeBuff(false, "Fortitude of the Bear"),
+                [171249] = makeBuff(true, "prot", "INSTANCE_CHAT", false, 116411),
+                [357650] = makeBuff(false, "mini BL"),
+                --[197916] = { false, "Lebenszyklus (Beleben)" },
+                --[197919] = { false, "Lebenszyklus (Einhüllender Nebel)" },
+                --[164273] = makeBuff(false, "Einsamer Wolf"),
+                --[2645] = { false, "Geisterwolf" },
+                --[61295] = makeBuff(false, "Springflut", nil, false, 116411) --}
+}
+local aurasMeta = {
+  __index = function (self, auraInstanceID)
+    return self.store[auraInstanceID]
+  end,
+  __newindex = function (self, auraInstanceID, pair)
+    self.store[auraInstanceID] = self.store[auraInstanceID] or {}
+    table.insert(self.store[auraInstanceID], pair)
+  end,
+  __call = function (self, auraInstanceID)
+    return table.remove(self.store[auraInstanceID], 1)
+  end
+}
+local auras = setmetatable({ store = {}, debug = false }, aurasMeta)
+
+-- /auaura
+SLASH_AU_AURA1 = "/auaura"
+
+SlashCmdList["AU_AURA"] = function (message, _editBox)
+  if message == "" then
+    print("dump auras")
+    DevTools_Dump(auras)
+    print("dump buffs")
+    DevTools_Dump(buffs)
+  end
+end
+
+local function debugAura(unitTarget, auraData)
+  print(format("LOCAL: %s start, target = %s, source = %s, spellId = %s", auraData.name, UnitName(unitTarget), auraData.sourceUnit, auraData.spellId))
+end
+
+function f:UNIT_AURA(event, unitTarget, updateInfo)
+  if not updateInfo then return end
+  if updateInfo.isFullUpdate then return end
+
+  if updateInfo.addedAuras and next(updateInfo.addedAuras) then
+    for _, auraData in ipairs(updateInfo.addedAuras) do
+      if auras.debug then debugAura(unitTarget, auraData) end
+
+      local buff = buffs[auraData.spellId]
+      if buff then
+        --debugAura(unitTarget, auraData)
+        local source = UnitName(auraData.sourceUnit)
+        if buff.banner then source = 'banner' end
+        auras[auraData.auraInstanceID] = { source, buff }
+
+        local message = format("%s used %s", source, buff.name)
+        if buff.track and UnitInBattleground("player") then
+          local message = format("{rt4} %s", message)
+          if buff.itemID then
+            message = format("%s (%s)", message, (select(2, GetItemInfo(buff.itemID))))
+          end
+          SendChatMessage(message, buff.channel)
+        end
+
+        print(format("LOCAL: %s, track? %s", message, tostring(buff.track)))
+      end
+    end
+  elseif updateInfo.updatedAuraInstanceIDs and next(updateInfo.updatedAuraInstanceIDs) then
+    for _, auraInstanceID in ipairs(updateInfo.updatedAuraInstanceIDs) do
+      
+    end
+  elseif updateInfo.removedAuraInstanceIDs and next(updateInfo.removedAuraInstanceIDs) then
+    for _, auraInstanceID in ipairs(updateInfo.removedAuraInstanceIDs) do
+      if auras[auraInstanceID] then
+        -- using __call
+        local source, buff = unpack(auras(auraInstanceID))
+        if not source or not buff then return end
+
+        if buff.track and not buff.banner and UnitInBattleground("player") then
+          -- "PARTY", "INSTANCE_CHAT", "SAY"
+          SendChatMessage(format("{rt7} %s expired", buff.name), buff.channel)
+        end
+        print(format("LOCAL: %s by %s expired", buff.name, source))
+      end
+    end
+  end
+end
+
+f:RegisterEvent("ADDON_LOADED")
+--f:RegisterEvent("UNIT_AURA")
+f:RegisterUnitEvent("UNIT_AURA", "player")
+f:SetScript("OnEvent", f.OnEvent)
---
+++ AshranUtils/Inferno.lua
@@ -0,0 +1,79 @@
+local f = CreateFrame("Frame")
+
+function f:OnEvent(event, ...)
+	self[event](self, event, ...)
+end
+
+function f:ADDON_LOADED(event, addOnName)
+  if addOnName == "AshranUtils" then
+    print(format("Hello %s! Inferno.lua loaded.", UnitName("player")))
+  end
+end
+
+local function isInfernoSpawned()
+  if GetRealZoneText() ~= "Ashran" then return false end
+
+  local mapID = C_Map.GetBestMapForUnit("player")
+  for _, areaPoiID in ipairs(C_AreaPoiInfo.GetAreaPOIForMap(mapID)) do
+    if areaPoiID == 6493 then return true end
+  end
+
+  return false
+end
+
+-- /dump C_AreaPoiInfo.GetAreaPOIInfo(1478, 6493)
+
+local function makeText(text)
+  RaidNotice_AddMessage(RaidWarningFrame, text, ChatTypeInfo["RAID_WARNING"])
+end
+
+local function announceInferno()
+  makeText("ancient inferno spawned")
+  if UnitInBattleground("player") and GetBattlefieldInstanceExpiration() == 0 then
+    -- INSTANCE_CHAT, RAID, PARTY, SAY (but then no icons)
+    SendChatMessage("{rt8} ancient inferno spawned", "INSTANCE_CHAT")
+  end
+end
+
+local ashran = { inferno = false }
+
+function f:ZONE_CHANGED_NEW_AREA()
+  -- print(format("entered zone %s", GetRealZoneText()))
+
+  if GetRealZoneText() == "Ashran" then
+    print(format("inferno spawned? %s", tostring(isInfernoSpawned())))
+  else
+    ashran.inferno = false
+  end
+end
+
+function f:AREA_POIS_UPDATED()
+  if GetRealZoneText() ~= "Ashran" then return end
+
+  local b = isInfernoSpawned()
+  -- you can see the moment of inferno death, will be true (is up) flip to false (dead)
+  print(format("AREA_POIS_UPDATED, inferno? %s", tostring(b)))
+  if b and not ashran.inferno then
+    announceInferno()
+    ashran.inferno = true
+  elseif not b and ashran.inferno then
+    ashran.inferno = false
+  end
+end
+
+function f:PVP_MATCH_COMPLETE(event, _winner, _duration)
+  ashran.inferno = false
+end
+
+SLASH_AU_AA1 = "/auaa"
+
+SlashCmdList["AU_AA"] = function (message, _editBox)
+  local b = isInfernoSpawned()
+  print(format("inferno? %s", tostring(b)))
+end
+
+f:RegisterEvent("ADDON_LOADED")
+f:RegisterEvent("AREA_POIS_UPDATED")
+f:RegisterEvent("PVP_MATCH_COMPLETE")
+f:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+f:SetScript("OnEvent", f.OnEvent)
