7a893a68c2c88240897ddcd1565e4ee4d111b2ea
bozo20 <bozo20@wow-addon.lua>
2023-09-03 18:25:39 +0200

    UI to track aura

--- Auras.lua
+++ Auras.lua
@@ -59,9 +59,23 @@ local function makeOnceExpiration(id, after, total)
     local aura = C_UnitAuras.GetPlayerAuraBySpellID(id)
     if not aura then return end
 
-    local message = format("%s in %d seconds!", aura.name, total - after)
+    -- DevTools_Dump(aura)
+    local collected = (aura.points[1] or 0) / 1000
+    local message = format("%s in %d seconds! %d k healing", aura.name, total - after, collected)
     RaidNotice_AddMessage(RaidWarningFrame, message, ChatTypeInfo["RAID_WARNING"])
     PlaySound(8959)
+    C_Timer.NewTicker(1, function (timer)
+      local aura = C_UnitAuras.GetPlayerAuraBySpellID(id)
+      if not aura then
+        timer:Cancel()
+        return
+      end
+
+      local collected = (aura.points[1] or 0) / 1000
+      local message = format("%s! %d k healing", aura.name, collected)
+      RaidNotice_AddMessage(RaidWarningFrame, message, ChatTypeInfo["RAID_WARNING"])
+      print(message)
+    end, total - after)
   end
   expiration.makeCallback = function ()
     C_Timer.After(after, callback)
@@ -91,15 +105,100 @@ local function makeRepeatingExpiration(id, after, announce)
   return expiration
 end
 
+local function makeDecreasingStatusBar(id, name)
+  local expiration = {}
+  expiration.makeCallback = function (auraData)
+    print("in makeDeacreasingStatusBar")
+    local maxValue = auraData.expirationTime - GetTime()
+    local name = name or auraData.name
+    local statusBar = _G["DecreasingStatusBar"] or CreateFrame("StatusBar", "DecreasingStatusBar", UIParent)
+    statusBar:SetPoint("CENTER", 0, -30)
+    statusBar:SetReverseFill(true)
+    statusBar:SetSize(100, 30)
+    statusBar:SetMinMaxValues(0, maxValue)
+    statusBar.texture = statusBar.texture or statusBar:CreateTexture()
+    statusBar.texture:SetColorTexture(255 / 255, 215 / 255, 0, 0.75)
+    statusBar:SetStatusBarTexture(statusBar.texture)
+    statusBar:SetValue(maxValue)
+    statusBar.fs = statusBar.fs or statusBar:CreateFontString(nil, "OVERLAY", "GameTooltipText")
+    statusBar.fs:SetPoint("CENTER")
+    statusBar.fs:SetText(format("%s: %d s left", name, maxValue))
+
+    local function callback(timer)
+      local aura = C_UnitAuras.GetPlayerAuraBySpellID(id)
+      if not aura then
+        statusBar:Hide()
+        timer:Cancel()
+        return
+      end
+
+      local remainingTime = math.floor(aura.expirationTime - GetTime())
+      statusBar.fs:SetText(format("%s: %d s left", name, remainingTime))
+      statusBar:SetValue(remainingTime)
+    end
+
+    local statusMin, statusMax = statusBar:GetMinMaxValues()
+    print(format("statusMin = %d, statusMax = %d", statusMin, statusMax))
+    statusBar:Show()
+    C_Timer.NewTicker(1, callback)
+  end
+
+  return expiration
+end
+
+local CloudburstHealing = "CloudburstHealing"
+local cbFormatString = "%d k heal"
+
+local function makeStatusBar(id)
+  local expiration = {}
+  expiration.makeCallback = function (auraData)
+    local statusBar = _G[CloudburstHealing] or CreateFrame("StatusBar", CloudburstHealing, UIParent)
+    -- statusBar:SetBackdrop(BACKDROP_ACHIEVEMENTS_0_64) BackdropTemplate , "AnimatedStatusBarTemplate"
+    statusBar:SetPoint("CENTER")
+    statusBar:SetSize(100, 30)
+    statusBar:SetMinMaxValues(0, 500)
+    --statusBar:SetStatusBarTexture("Interface\\TARGETINGFRAME\\UI-StatusBar")
+    local statusMin, statusMax = statusBar:GetMinMaxValues()
+    print(format("statusMin = %d, statusMax = %d", statusMin, statusMax))
+    statusBar.fs = statusBar.fs or statusBar:CreateFontString(nil, "OVERLAY", "GameTooltipText")
+    statusBar.fs:SetPoint("CENTER")
+    statusBar.fs:SetText(format(cbFormatString, 0))
+
+    statusBar.texture = statusBar.texture or statusBar:CreateTexture()
+    -- statusBar.texture:SetAllPoints()
+    statusBar.texture:SetColorTexture(127 / 255, 255 / 255, 0, 0.75)
+    statusBar:SetStatusBarTexture(statusBar.texture)
+    statusBar:SetValue(0)
+
+    local function callback(timer)
+      local aura = C_UnitAuras.GetPlayerAuraBySpellID(id)
+      if not aura then
+        statusBar:Hide()
+        timer:Cancel()
+        return
+      end
+      statusBar:Show()
+  
+      local collected = math.floor((aura.points[1] or 0) / 1000)
+      statusBar:SetValue(collected)
+      statusBar.fs:SetText(format(cbFormatString, collected))
+    end
+
+    C_Timer.NewTicker(1, callback)
+  end
+
+  return expiration
+end
+
 local expirations = {
   -- cloudburst
-  [157504] = makeOnceExpiration(157504, 10, 15),
+  [157504] = { makeOnceExpiration(157504, 10, 15), makeStatusBar(157504) },
   -- Springflut
-  --[61295] = makeRepeatingExpiration(61295, 6),
+  [61295] = { makeDecreasingStatusBar(61295) },
   -- prot
-  [171249] = makeRepeatingExpiration(171249, 5, not true),
+  [171249] = { makeDecreasingStatusBar(171249, "prot") },
   -- speed
-  [171250] = makeRepeatingExpiration(171250, 5, not true)
+  [171250] = { makeRepeatingExpiration(171250, 5, not true) }
 }
 local aurasMeta = {
   __index = function (self, auraInstanceID)
@@ -165,16 +264,21 @@ function f:UNIT_AURA(event, unitTarget, updateInfo)
         debugPrint(format("LOCAL: %s, track? %s", message, tostring(buff.track)))
       end
 
-      local expiration = expirations[auraData.spellId]
-      if expiration then
-        expiration.makeCallback()
+      local list = expirations[auraData.spellId]
+      if list then
+        for _, expiration in ipairs(list) do
+          expiration.makeCallback(auraData)
+        end
       end
     end
-  elseif updateInfo.updatedAuraInstanceIDs and next(updateInfo.updatedAuraInstanceIDs) then
+  end
+ 
+  if updateInfo.updatedAuraInstanceIDs and next(updateInfo.updatedAuraInstanceIDs) then
     for _, auraInstanceID in ipairs(updateInfo.updatedAuraInstanceIDs) do
-      
     end
-  elseif updateInfo.removedAuraInstanceIDs and next(updateInfo.removedAuraInstanceIDs) then
+  end
+ 
+  if updateInfo.removedAuraInstanceIDs and next(updateInfo.removedAuraInstanceIDs) then
     for _, auraInstanceID in ipairs(updateInfo.removedAuraInstanceIDs) do
       if auras[auraInstanceID] then
         -- using __call
