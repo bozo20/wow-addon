4f36ae9a2661d0fe02d1549f67076f87b92587ca
bozo20 <bozo20@wow-addon.lua>
2023-09-20 08:36:59 +0200

    random mounts

--- Mounts.lua
+++ Mounts.lua
@@ -29,7 +29,6 @@ local function makeLookupMap(...)
   return t
 end
 
-
 local typeMap = makeLookupMap({ 230, "ground" },
                               { 248, "flying" },
                               { 402, "advflying" })
@@ -43,7 +42,9 @@ do
       dismount = { ns.hex2rgb("ff0000") },
       ground = { ns.hex2rgb("bd396d") },
       advflying = { ns.hex2rgb("d0d938") },
-      flying = { ns.hex2rgb("64ab22") }
+      flying = { ns.hex2rgb("64ab22") },
+      swimming = { ns.hex2rgb("64ab22") },
+      zoned = { ns.hex2rgb("64ab22") }
     },
     __call = function (self, typeOrName)
       local argType = type(typeOrName)
@@ -54,89 +55,114 @@ do
       end
     end
   }
-  
+
   setmetatable(colours, coloursMeta)
 end
 
+local function makeIsMapFunction(targetMapID)
+  return function ()
+    local mapID = C_Map.GetBestMapForUnit("player")
+    local mapInfo = C_Map.GetMapInfo(mapID)
+    if mapID == targetMapID then return true end
+
+    while mapInfo.mapID ~= targetMapID or mapInfo.parentMapID ~= 0 do
+      print("in step")
+      mapInfo = C_Map.GetMapInfo(mapInfo.parentMapID)
+      if not mapInfo then return false end
+      if mapInfo.mapID == targetMapID then return true end
+    end
+    print("fell through")
+    return false
+  end
+end
+
+local isVashjir = makeIsMapFunction(203)
+local isAhnQiraj = makeIsMapFunction(320)
 
 local mounts = {}
 do
+  local types = {
+    ["ground"] = { [230] = true, [412] = true },
+    ["flying"] = { [247] = true, [248] = true, [424] = true },
+    -- 424 NOT YET
+    ["advflying"] = { [402] = true, [424] = not true },
+    ["swimming"] = { [231] = true, [407] = true },
+    ["zoned"] = { [232] = isVashjir,  [241] = isAhnQiraj }
+  }
   local mountsMeta = {
     __index = {
-      types = { [230] = {},
-                [248] = {},
-                [402] = {} },
-      isMountUsable = function (self, isFactionSpecific, faction)
+      debug = function (self)
+        for mountType, mounts in pairs(self.lists) do
+          print(format("mounts2: #%s = %d", mountType, #mounts))
+        end
+      end,
+      isFactionUsable = function (self, isFactionSpecific, faction)
         if not isFactionSpecific then return true end
       
         local englishFaction, localizedFaction = UnitFactionGroup("player")
-    
+
         return factionMap[englishFaction] == faction
       end,
-      debug = function (self)
-        for mountTypeID, mounts in pairs(self.types) do
-          print(format("#%s = %d", typeMap[mountTypeID], #mounts))
-        end
-      end,
-      isZoneForDragonriding = function (self)
-        local mapID = C_Map.GetBestMapForUnit("player")
-        local mapInfo = C_Map.GetMapInfo(mapID)
-        while mapInfo.mapID ~= 1978 and mapInfo.parentMapID ~= 0 do
-          mapInfo = C_Map.GetMapInfo(mapInfo.parentMapID)
-          if mapInfo.mapID == 1978 then return true end
-        end
-  
-        return false
-      end,
       random = function (self, type)
-        local meta = getmetatable(self)
-        local mountTypeID = typeMap[type]
-  
-        local mountID, name, isFactionSpecific, faction
-        local max = #self.types[mountTypeID]
-        local canUse, tries, randomIndex = false, 0, math.random(max)
-        for i = randomIndex, randomIndex + max do
-          local index = i % max + 1
-          mountID, name, isFactionSpecific, faction = unpack(self.types[mountTypeID][index])
-          canUse = self:isMountUsable(isFactionSpecific, faction)
+        local mounts = self.lists[type]
 
-          if canUse then break end
-          tries = tries + 1
-  
-          if isDebug() then
-            ns.print(format("skipped: %s, faction = %s", name, tostring(factionMap[faction])), colours(type))
+        local max = #mounts
+        local randomIndex = math.random(max)
+        local mountID, name
+        local skipped = 0
+        for i = randomIndex, randomIndex + max do
+          local index = i % (max + 1)
+          index = math.max(index, 1)
+          mountID, name = unpack(mounts[index])
+          local name, spellID, icon, isActive, isUsable, sourceType, isFavorite, isFactionSpecific, faction, shouldHideOnChar, isCollected, mountID, isForDragonriding = C_MountJournal.GetMountInfoByID(mountID)
+          if isActive then
+            isUsable = false
+          end
+          if isUsable then break end
+
+          print(format("skipped: %s (id = %d, typeID = %d)", name, mountID, mountTypeID))
+          skipped = skipped + 1
+        end
+        ns.print(format("summoning %s (%s, start: %d, skipped: %d)", name, type, randomIndex, skipped), colours(type))
+        C_MountJournal.SummonByID(mountID)
+      end,
+      lists = {
+        ["ground"] = {},
+        ["flying"] = {},
+        ["advflying"] = {},
+        ["swimming"] = {},
+        ["zoned"] = {}
+      }
+    },
+    __call = function (self, mountTypeID, mountID, name)
+      if self[mountID] then return end
+
+      self[mountID] = true
+      for k, v in pairs(types) do
+        local mapping = v[mountTypeID]
+        if mapping then
+          local mappingType = type(mapping)
+          if mappingType == "boolean" then
+            table.insert(self.lists[k], { mountID, name })
           end
         end
-        if not canUse then print("No mount at all!") return end
-  
-        ns.print(format("summoning %s (%s, start: %d, tries: %d)", name, type, randomIndex, tries), colours(type))
-        C_MountJournal.SummonByID(mountID)
       end
-    },
-    __call = function (self, mountTypeID, mountID, name, isFactionSpecific, faction)
-      if not self.types[mountTypeID] then return end
-      if self[mountID] then return end
-  
-      self[mountID] = mountID
-      table.insert(self.types[mountTypeID], { mountID, name, isFactionSpecific, faction })
     end
   }
+
   setmetatable(mounts, mountsMeta)
 end
 
 local function readMounts()
-  if mounts.scanned then return end
-
-  local any = false
   for _, mountID in ipairs(C_MountJournal.GetMountIDs()) do
-    local name, spellID, icon, isActive, isUsable, sourceType, isFavorite, isFactionSpecific, faction, shouldHideOnChar, isCollected, mountID, isForDragonriding = C_MountJournal.GetMountInfoByID(mountID)
-    if isCollected then
-      local creatureDisplayInfoID, description, source, isSelfMount, mountTypeID, uiModelSceneID, animID, spellVisualKitID, disablePlayerMountPreview = C_MountJournal.GetMountInfoExtraByID(mountID)
-      mounts(mountTypeID, mountID, name, isFactionSpecific, faction)
-      any = true
+    if not mounts[mountID] then
+      local name, spellID, icon, isActive, isUsable, sourceType, isFavorite, isFactionSpecific, faction, shouldHideOnChar, isCollected, mountID, isForDragonriding = C_MountJournal.GetMountInfoByID(mountID)
+      if isCollected and mounts:isFactionUsable(isFactionSpecific, faction) then
+        local creatureDisplayInfoID, description, source, isSelfMount, mountTypeID, uiModelSceneID, animID, spellVisualKitID, disablePlayerMountPreview = C_MountJournal.GetMountInfoExtraByID(mountID)
+        mounts(mountTypeID, mountID, name)
+      end
     end
   end
-  mounts.scanned = any
 end
 
 function f:ADDON_LOADED(event, addOnName)
@@ -150,22 +176,62 @@ function f:MOUNT_JOURNAL_USABILITY_CHANGED(event)
   readMounts()
 end
 
+function f:NEW_MOUNT_ADDED(event, mountID)
+  local name, spellID, icon, isActive, isUsable, sourceType, isFavorite, isFactionSpecific, faction, shouldHideOnChar, isCollected, mountID, isForDragonriding = C_MountJournal.GetMountInfoByID(mountID)
+  local creatureDisplayInfoID, description, source, isSelfMount, mountTypeID, uiModelSceneID, animID, spellVisualKitID, disablePlayerMountPreview = C_MountJournal.GetMountInfoExtraByID(mountID)
+  mounts(mountTypeID, mountID, name)
+end
+
+
 SLASH_AU_MOUNT1 = "/aumount"
 
 SlashCmdList["AU_MOUNT"] = function (message, editBox)
   readMounts()
 
-  if IsIndoors() then return end
+  if IsIndoors() then ns.print("Indoors!", colours("dismount")) return end
+  if IsSwimming() or message == "swimming" then mounts:random("swimming") return end
 
   -- print(format("IsAltKeyDown() = %s, IsShiftKeyDown() = %s", tostring(IsAltKeyDown()), tostring(IsShiftKeyDown())))
   if message == "info" then
     ns.print("Mounts info:")
     mounts:debug()
+--[[
+    print(format("Vashj'ir? %s", tostring(isVashjir())))
+    print(format("Ahn'Qiraj? %s", tostring(isAhnQiraj())))
+    local mountTypes = {}
+    for _, mountID in ipairs(C_MountJournal.GetMountIDs()) do
+      local name, spellID, icon, isActive, isUsable, sourceType, isFavorite, isFactionSpecific, faction, shouldHideOnChar, isCollected, mountID, isForDragonriding = C_MountJournal.GetMountInfoByID(mountID)
+      if isCollected and mounts:isFactionUsable(isFactionSpecific, faction) then
+        local creatureDisplayInfoID, description, source, isSelfMount, mountTypeID, uiModelSceneID, animID, spellVisualKitID, disablePlayerMountPreview = C_MountJournal.GetMountInfoExtraByID(mountID)
+        mountTypes[mountTypeID] = mountTypes[mountTypeID] or {}
+        table.insert(mountTypes[mountTypeID], mountID)
+
+        if isActive then
+          print(format("mounted on %s (%d, type: %d)", name, mountID, mountTypeID))
+        end
+      end
+    end
+    local mountTypeIDs = {}
+    for mountTypeID, mounts in pairs(mountTypes) do
+      table.insert(mountTypeIDs, mountTypeID)
+      -- print(format("mountTypeID %d: #mounts = %d", mountTypeID, #mounts))
+    end
+    table.sort(mountTypeIDs)
+    for _, mountTypeID in ipairs(mountTypeIDs) do
+      local mounts = mountTypes[mountTypeID]
+      print(format("mountTypeID %d: #mounts = %d", mountTypeID, #mounts))
+      if #mounts < 10 then
+        print(format("  mountIDs = %s", table.concat(mounts, ", ")))
+      end
+    end
+--]]
   elseif IsAltKeyDown() then
     mounts:random("ground")
   elseif IsShiftKeyDown() then
     ns.print("Dismount!", colours("dismount"))
     Dismount()
+  elseif IsControlKeyDown() then
+    mounts:random("flying")
   elseif IsAdvancedFlyableArea() then
     mounts:random("advflying")
   else
@@ -175,4 +241,5 @@ end
 
 f:RegisterEvent("ADDON_LOADED")
 f:RegisterEvent("MOUNT_JOURNAL_USABILITY_CHANGED")
+f:RegisterEvent("NEW_MOUNT_ADDED")
 f:SetScript("OnEvent", f.OnEvent)
